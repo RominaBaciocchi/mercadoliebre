267b88d79af490b4854c94d3698ed60d6537016d
rominabaciocchi <rominabaciocchi@gamil.com>
2021-07-29 17:33:30 -0300

    Agrego proyecto

---
+++ app.js
@@ -0,0 +1,27 @@
+const { response } = require('express');
+const express = require ('express');
+const app = express ();
+const port = 3030;
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+
+app.listen(3030,()=>{
+    console.log('La app esta funcionando en el puerto http://localhost:' + port);
+});
+
+app.get('/', function(req,res){
+res.sendFile(path.resolve(__dirname, 'views/index.html'))
+});
+app.get('/register', function(req,res){
+    res.sendFile(path.resolve(__dirname, 'views/register.html'))
+    });
+
+app.get('/login', function(req,res){
+        res.sendFile(path.resolve(__dirname, 'views/login.html'))
+    });    
+
+
+app.use(express.static('public'));
